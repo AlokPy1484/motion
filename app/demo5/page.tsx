cd6245ea2a39cfb3803167f2270d4b7f687fe8e6
AlokPy1484 <alokpandey1483@gmail.com>
2026-06-10 20:27:53 +0530

    portfolio button kinda completed

---
+++ app/demo5/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import nextLogo from "../../public/globe.svg"
+import Lession2 from "@/components/Lession2";
+
+
+
+
+export default function page(){
+
+
+    return(
+            <div className="flex justify-center items-center w-screen h-screen">
+
+                <Lession2/>
+            </div>
+    )
+}
--- components/motion-button.tsx
+++ components/motion-button.tsx
@@ -1,4 +1,5 @@
-
+"use client"
+import {color, motion, scale} from "motion/react"
 
 
 
@@ -11,16 +12,80 @@
 
 export default function MotionButton(){
 
+    const parentVarient = {
+        initial: {
+            scale: 1
+        },
+        hover: {
+            scale: 1.1
+        }
+    }
+
+    const childVarient = {
+        initial: {
+                rotate: -30,
+                translateY: "-80px",
+                translateX: "-20px"
+            },
+        hover: {
+                rotate: 0,
+                translateY: 0,
+                translateX: 0
+            }
+
+    }
+
+    const textVarient = {
+        initial: {
+            color: "#ffffff"
+            
+        },
+        hover: {
+            color: "#000000"
+        }
+    }
+    
+
 
 
     return(
 
-        <div className="px-8 py-4 bg-black text-sm  text-white"
-         style={{
-  backgroundImage:
-    "repeating-linear-gradient(135deg, rgba(255,255,255,0.2) 0, rgba(255,255,255,0.2) 1px, transparent 1px, transparent 8px) ",
-}}>
-                Hover Over
-        </div>
+   
+
+        <motion.div
+        variants={parentVarient}
+        initial="initial"
+        whileHover="hover"
+        transition={{
+                duration: 0.3,
+                ease: "easeInOut"
+            }}
+
+        
+        className="group relative flex w-30 h-8 justify-center items-center bg-black text-sm  text-white overflow-hidden"
+          style={{
+        backgroundImage:
+            "repeating-linear-gradient(135deg, rgba(255,255,255,0.2) 0, rgba(255,255,255,0.2) 1px, transparent 1px, transparent 8px) ",
+        }}>
+            
+
+            <motion.span
+            variants={childVarient}
+
+            transition={{
+                duration: 0.3,
+                ease: "easeInOut"
+            }}
+
+            
+            className="absolute inset-0 w-full h-full bg-white"></motion.span>
+
+            <motion.div 
+            variants={textVarient}
+            className="relative z-10 text-xl">Alok</motion.div>
+
+        </motion.div>
+
+
     )
 }
